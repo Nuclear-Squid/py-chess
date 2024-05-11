a4cdfce178d1011d5b8b52b63c32039ca47f19bb
Nuclear Squid <leo@cazenave.cc>
2024-05-11 18:44:04 +0200

    Pieces can move.

--- backend/lib.c
+++ backend/lib.c
@@ -13,7 +13,10 @@ static ChessBoard main_chess_board = {
     { {WHITE, ROOK}, {WHITE, KNIGHT}, {WHITE, BISHOP}, {WHITE, QWEEN}, {WHITE, KING}, {WHITE, BISHOP}, {WHITE, KNIGHT}, {WHITE, ROOK} },
 };
 
+static PieceColor color_to_play = WHITE;
+
 ChessBoard* get_main_chess_board() { return &main_chess_board; }
+PieceColor get_color_to_play() { return color_to_play; }
 
 static void log_position(Position pos) {
     printf("(col: %hhu, row: %hhu)\n", pos.col, pos.row);
@@ -33,7 +36,7 @@ static inline Position mul_position(Position pos, i8 multiplier) {
     };
 }
 
-static inline Piece get_piece_at(ChessBoard board, Position pos) {
+Piece get_piece_at(ChessBoard board, Position pos) {
     return board[pos.row][pos.col];
 }
 
@@ -41,104 +44,150 @@ static inline CellState get_cell_state(ChessBoard board, Position cell_pos, Piec
     if (cell_pos.col >= 8 || cell_pos.row >= 8) return OUT_OF_BOUNDS;
     const Piece other_piece = get_piece_at(board, cell_pos);
     if (other_piece.type == EMPTY) return FREE;
-    printf("%hhu, %hhu\n", other_piece.color, piece_color);
     return other_piece.color == piece_color ? SAME_COLOR : OTHER_COLOR;
 }
 
-static void show_moves_line(ChessBoard board, Position pos, Direction dir, PieceColor color) {
+static size_t get_moves_line(ChessBoard board, Position pos, Direction dir, PieceColor color, Position* output) {
     // FIXME: This is fucking disgusting.
     Position aimed_cell;
     CellState aimed_cell_state;
     i32 i = 0;
 
     while ((aimed_cell_state = get_cell_state(board, aimed_cell = add_positions(pos, mul_position(dir, ++i)), color)) == FREE)
-        log_position(aimed_cell);
-    if (aimed_cell_state == OTHER_COLOR) log_position(aimed_cell);
+        output[i - 1] = aimed_cell;
+
+    if (aimed_cell_state == OTHER_COLOR) {
+        output[i - 1] = aimed_cell;
+        return i;
+    }
+
+    return i - 1;
 }
 
-static void show_move_if_valid(ChessBoard board, Position pos, Direction dir, PieceColor color) {
+static size_t get_move_if_valid(ChessBoard board, Position pos, Direction dir, PieceColor color, Position* output) {
     Position aimed_cell = add_positions(pos, dir);
     switch (get_cell_state(board, aimed_cell, color)) {
         case FREE:
         case OTHER_COLOR:
-            return log_position(aimed_cell);
+            *output = aimed_cell;
+            return 1;
 
         case SAME_COLOR:
         case OUT_OF_BOUNDS:
-            break;
+            return 0;
     }
 }
 
-void show_possible_moves(ChessBoard board, Position pos) {
-    printf("-----------\n");
+size_t get_possible_moves(ChessBoard board, Position pos, Position* output) {
     const Piece piece = get_piece_at(board, pos);
     Direction pawn_direction = piece.color == WHITE
         ? (Direction) { .row = -1, .col =  0 }
         : (Direction) { .row =  1, .col =  0 };
 
+    size_t nb_moves = 0;
+
     switch (piece.type) {
         case EMPTY: break;
         case PAWN:
-            log_position(add_positions(pos, pawn_direction));
+            output[nb_moves++] = add_positions(pos, pawn_direction);
             if (pos.row == 1 || pos.row == 6)
-                log_position(add_positions(pos, mul_position(pawn_direction, 2)));
+                output[nb_moves++] = add_positions(pos, mul_position(pawn_direction, 2));
 
             pawn_direction.col = 1;
             Position aimed_cell = add_positions(pos, pawn_direction);
             if (get_cell_state(board, aimed_cell, piece.color) == OTHER_COLOR)
-                log_position(aimed_cell);
+                output[nb_moves++] = aimed_cell;
 
             pawn_direction.col = -1;
             aimed_cell = add_positions(pos, pawn_direction);
             if (get_cell_state(board, aimed_cell, piece.color) == OTHER_COLOR)
-                log_position(aimed_cell);
+                output[nb_moves++] = aimed_cell;
             break;
 
         case ROOK:
-            show_moves_line(board, pos, (Direction) { .row = -1, .col =  0 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  1, .col =  0 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  0, .col = -1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  0, .col =  1 }, piece.color);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row = -1, .col =  0 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  1, .col =  0 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  0, .col = -1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  0, .col =  1 }, piece.color, output + nb_moves);
             break;
 
         case KNIGHT:
-            show_move_if_valid(board, pos, (Direction) { .row =  1, .col =  2 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row = -1, .col =  2 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row =  1, .col = -2 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row = -1, .col = -2 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row =  2, .col =  1 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row =  2, .col = -1 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row = -2, .col =  1 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row = -2, .col = -1 }, piece.color);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  1, .col =  2 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row = -1, .col =  2 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  1, .col = -2 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row = -1, .col = -2 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  2, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  2, .col = -1 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row = -2, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row = -2, .col = -1 }, piece.color, output + nb_moves);
             break;
 
         case BISHOP:
-            show_moves_line(board, pos, (Direction) { .row =  1, .col =  1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  1, .col = -1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row = -1, .col =  1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row = -1, .col = -1 }, piece.color);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  1, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  1, .col = -1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row = -1, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row = -1, .col = -1 }, piece.color, output + nb_moves);
             break;
 
         case QWEEN:
-            show_moves_line(board, pos, (Direction) { .row = -1, .col =  0 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  1, .col =  0 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  0, .col = -1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  0, .col =  1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  1, .col =  1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row =  1, .col = -1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row = -1, .col =  1 }, piece.color);
-            show_moves_line(board, pos, (Direction) { .row = -1, .col = -1 }, piece.color);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row = -1, .col =  0 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  1, .col =  0 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  0, .col = -1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  0, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  1, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row =  1, .col = -1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row = -1, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_moves_line(board, pos, (Direction) { .row = -1, .col = -1 }, piece.color, output + nb_moves);
             break;
 
         case KING:
-            show_move_if_valid(board, pos, (Direction) { .row = -1, .col =  0 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row =  1, .col =  0 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row =  0, .col = -1 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row =  0, .col =  1 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row =  1, .col =  1 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row =  1, .col = -1 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row = -1, .col =  1 }, piece.color);
-            show_move_if_valid(board, pos, (Direction) { .row = -1, .col = -1 }, piece.color);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row = -1, .col =  0 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  1, .col =  0 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  0, .col = -1 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  0, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  1, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row =  1, .col = -1 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row = -1, .col =  1 }, piece.color, output + nb_moves);
+            nb_moves += get_move_if_valid(board, pos, (Direction) { .row = -1, .col = -1 }, piece.color, output + nb_moves);
             break;
     }
+
+    return nb_moves;
+}
+
+void play_move(ChessBoard board, Position start, Position end) {
+    board[end.row][end.col] = board[start.row][start.col];
+    board[start.row][start.col].type = EMPTY;
+    color_to_play = color_to_play == WHITE ? BLACK : WHITE;
+}
+
+void debug_log_chess_board(ChessBoard board) {
+    printf("+----\n");
+
+    for (size_t row = 0; row < 8; row++) {
+        printf("%c", row < 2 ? '|' : ' ');
+
+        for (size_t col = 0; col < 8; col++) {
+            Piece current_piece = get_piece_at(board, (Position) { col, row });
+            if (current_piece.type == EMPTY) {
+                printf(" --");
+                continue;
+            }
+
+            printf(" %c", current_piece.color == WHITE ? 'w' : 'b');
+            switch (current_piece.type) {
+                case EMPTY: break;  // already handled in the code above
+                case PAWN:   printf("p"); break;
+                case ROOK:   printf("r"); break;
+                case KNIGHT: printf("h"); break;  // 'h' for "horse"
+                case BISHOP: printf("b"); break;
+                case QWEEN:  printf("q"); break;
+                case KING:   printf("k"); break;
+            }
+        }
+
+        printf("%c\n", row >= 6 ? '|' : ' ');
+    }
+
+    printf("                     ----+\n");
 }
--- frontend/main.py
+++ frontend/main.py
@@ -7,22 +7,65 @@ from enum import Enum
 class PieceColor(Enum):
     WHITE, BLACK = range(2)
 
+    @classmethod
+    def from_param(cls, obj):
+        return obj
+
+    def __str__(self):
+        match self:
+            case PieceColor.WHITE: return "white"
+            case PieceColor.BLACK: return "black"
+            case other: raise ValueError(f"Unknown color {other}")
+
+    def __repr__(self):
+        return self.__str__()
+
 class PieceType(Enum):
     EMPTY, PAWN, ROOK, KNIGHT, BISHOP, QWEEN, KING = range(7)
+
+    def __str__(self):
+        match self:
+            case PieceType.EMPTY:  return "empty"
+            case PieceType.PAWN:   return "pawn"
+            case PieceType.ROOK:   return "rook"
+            case PieceType.KNIGHT: return "knight"
+            case PieceType.BISHOP: return "bishop"
+            case PieceType.QWEEN:  return "qween"
+            case PieceType.KING:   return "king"
+            case other: raise ValueError(f"Unknown piece type {other}")
+
+    def __repr__(self):
+        return self.__str__()
 
 class Piece(ctypes.Structure):
     _fields_ = [("color", ctypes.c_uint8, 1), ("type", ctypes.c_uint8, 3)]
 
+    def __str__(self):
+        return f"{PieceColor(self.color)}-{PieceType(self.type)}"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.color == other.color and self.type == other.type
+
 class Position(ctypes.Structure):
     _fields_ = [("col", ctypes.c_uint8), ("row", ctypes.c_uint8)]
 
-    @classmethod
-    def new(cls, col, row):
-        return cls(ctypes.c_uint8(col), ctypes.c_uint8(row))
+    def __str__(self):
+        return f"(col: {self.col}, row: {self.row})"
 
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.col == other.col and self.row == other.row
 
 LIBCHESS = ctypes.CDLL("zig-out/lib/libchess.so")
 LIBCHESS.get_main_chess_board.restype = ctypes.POINTER((Piece * 8) * 8)
+LIBCHESS.get_possible_moves.restype = ctypes.c_size_t
+LIBCHESS.get_color_to_play.restype = PieceColor
+LIBCHESS.get_piece_at.restype = Piece
 
 class ChessBoardWidget(tk.Canvas):
     LIGHT_COLOR = "#cccccc"
@@ -32,14 +75,28 @@ class ChessBoardWidget(tk.Canvas):
         super().__init__(parent, width=size, height=size)
 
         self.cell_size = size // 8
+        self.moves_buffer = []
+        self.selected_cell = None
 
-        self.bind("<Button-1>", lambda event:
-            LIBCHESS.show_possible_moves(
-                LIBCHESS.get_main_chess_board().contents,
-                Position.new(event.x // self.cell_size, event.y // self.cell_size)
-            )
-        )
+        # TODO: Render the pieces on the board
 
+        def clicked_cell(event):
+            board = LIBCHESS.get_main_chess_board().contents
+            cell_clicked = Position(event.x // self.cell_size, event.y // self.cell_size)
+            color_to_play = LIBCHESS.get_color_to_play()
+
+            if cell_clicked in self.moves_buffer:
+                LIBCHESS.play_move(board, self.selected_cell, cell_clicked)
+                return
+
+            if LIBCHESS.get_piece_at(board, cell_clicked).color == color_to_play.value:
+                self.selected_cell = cell_clicked
+                self.moves_buffer = (Position * 24)()
+                nb_moves = LIBCHESS.get_possible_moves(board, cell_clicked, self.moves_buffer)
+                self.moves_buffer = self.moves_buffer[:nb_moves]
+                return
+
+        self.bind("<Button-1>", clicked_cell)
 
         for col in range(8):
             for row in range(8):
